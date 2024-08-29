82baa3e55b7cd62bb4da6848f4be1348689fa2b7
ri0103 <ryu2006ishizaki@gmail.com>
2024-08-29 11:32:10 +0900

    api1

---
+++ app/src/main/java/app/ishizaki/dragon/weatherforecast/WeatherResponse.kt
@@ -0,0 +1,17 @@
+package app.ishizaki.dragon.weatherforecast
+
+import com.squareup.moshi.Json
+
+class WeatherResponse (
+    val title: String = "",
+    @Json(name = "forecasts") val forecast: List<ForecastResponse>,
+)
+
+class ForecastResponse(
+    @Json(name = "telop") val weather: String,
+    @Json(name = "image") val images: ImageResponse,
+)
+class ImageResponse(
+    val title: String,
+    val url: String
+)
